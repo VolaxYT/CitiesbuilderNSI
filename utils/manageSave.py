fa91d7d8ab8f43c62a75c6f785bfe4f05950efff
VolaxYT <volax6600mailpro@gmail.com>
2023-01-07 00:47:25 +0100

    patch

--- utils/manageSave.py
+++ utils/manageSave.py
@@ -1,15 +1,5 @@
 from batiments.immeuble.immeuble import Immeuble
 
-def parse_tuple(string):
-    try:
-        s = eval(string)
-        if type(s) == tuple:
-            return s
-        return
-    except:
-        return
-
-
 def exportFile(fileName, objects):
     f = open("saves/" + fileName, "w")
     f.write(objects)
@@ -22,8 +12,7 @@ def importFile(fileName):
             parseImmeuble(line)
 
 def parseImmeuble(immeubleString):
-    print(immeubleString)
     options = immeubleString.replace('"', '').split(';')
-    immeuble = Immeuble(options[1], int(options[2]), float(options[3]), float(options[4]), int(options[5]), parse_tuple(options[6]), parse_tuple(options[7]), str(options[8]), list(options[9].replace('[', '').replace(']', '').replace(",", '').replace(" ", "")))
+    immeuble = Immeuble(options[1], int(options[2]), float(options[3]), float(options[4]), int(options[5]), eval(options[6]), eval(options[7]), str(options[8]), list(options[9].replace('[', '').replace(']', '').replace(",", '').replace(" ", "")))
     immeuble.draw()
 
