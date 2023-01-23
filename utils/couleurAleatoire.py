b1d6f76eee365479d591757bf5f8db55b63509b1
VolaxYT <volax6600mailpro@gmail.com>
2023-01-23 20:46:13 +0100

    new preset color palette

--- utils/couleurAleatoire.py
+++ utils/couleurAleatoire.py
@@ -1,7 +1,24 @@
 # module générer une couleur aléatoire
 
 import turtle
+from rectangle import rectangle
 from random import randint
+
+def gen_col_grey():
+    col = randint(0,200)
+    return col, col, col
+
+def gen_col_red():
+    col = randint(0,255)
+    return 255, col, col
+
+def gen_col_green():
+    col = randint(0,255)
+    return col, 255, col
+
+def gen_col_blue():
+    col = randint(0,255)
+    return col, col, 255
 
 def couleurAleatoire():
     '''
@@ -14,10 +31,11 @@ def couleurAleatoire():
 
 if __name__ == '__main__':
     turtle.colormode(255)
-    for i in range (15):
-        col=couleurAleatoire()
-        turtle.pencolor(col)
-        turtle.forward(150)
-        turtle.right(180-12)
+    col = gen_col_green()
+    turtle.pencolor(col)
+    turtle.begin_fill()
+    turtle.fillcolor(col)
+    rectangle(100, 100, 100, 100)
+    turtle.end_fill()
     # On ferme la fenêtre s'il y a un clique gauche
     turtle.exitonclick()
